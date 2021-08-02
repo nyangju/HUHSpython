921ff4347828d0196abf89f978d4eaeeaacb70bd
nyangju <81475397+nyangju@users.noreply.github.com>
2021-08-02 22:14:47 +0900

    Delete quiz3.py

--- quiz3.py
+++
@@ -1,6 +0,0 @@
-url = "http://naver.com"
-rule1 = url.replace("http://", "")
-rule1 = rule1[:rule1.index(".")]
-# print(rule1)
-rule2 = rule1[:3] + str(len(rule1)) + str(rule1.count("e")) + "!"
-print("{0} 의 비밀번호는 {1} 입니다." .format(url , rule2))
